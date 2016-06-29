e893236a6947c7eb071c51787483db0e9f5aad3d
DoZ10 <mlemay@gosecure.ca>
2016-06-29 11:14:12 -0400

    add: Lansweeper 6 support & fixed naming conventions.

---
+++ LPR6/PasswordRecovery.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lansweeper6_PasswordRecovery
+{
+    class PasswordRecovery
+    {
+        private string KeyFilename;
+        private string CipherFilename;
+        private byte[] Key;
+        private Dictionary<string, string> Credentials;
+        private CryptoClass Crypto;
+
+        public PasswordRecovery(string keyFilename, string cipherFilename)
+        {
+            KeyFilename = keyFilename;
+            CipherFilename = cipherFilename;
+            Key = new byte[1024];
+            Credentials = new Dictionary<string, string>();
+            Crypto = null;
+        }
+
+        public bool LoadFiles()
+        {
+            bool res = true;
+            if (KeyFilename == null || CipherFilename == null)
+                return res;
+
+            Console.WriteLine(string.Format("[*] Loading key file {0}.", KeyFilename));
+            Key = File.ReadAllBytes(KeyFilename);
+            Console.WriteLine(string.Format("[*] Processing {0} bytes for the key file.", Key.Length));
+            if (Key != null && Key.Length > 0)
+                Crypto = new CryptoClass(Key);
+            /* 
+             * Cipher File should consist of user:password delimited by comma.
+             * Passwords should be base64 encoded.
+             * Credentials should be one per line.
+             */
+            Console.WriteLine(string.Format("[*] Loading cipher file {0}", CipherFilename));
+            foreach (string s in File.ReadAllLines(CipherFilename))
+            {
+                Console.WriteLine(string.Format("[*] Loading cipher line {0}", s));
+                Credentials.Add(s.Split(':')[0],
+                    s.Split(':')[1]);
+            }
+            res = false;
+            return res;
+        }
+
+        public void DecryptAll()
+        {
+            if (Crypto == null)
+            {
+                Console.WriteLine("[x] Error processing the key file...quit.");
+                return;
+            }
+
+            string password;
+
+            foreach(KeyValuePair<string, string> kv in Credentials)
+            {
+                password = "";
+                password = kv.Value;
+
+                Console.WriteLine(string.Format("[-] Recovered password for user {0} as {1}",
+                    kv.Key,
+                    Crypto.Decrypt(kv.Value)
+                    ));
+            }
+        }
+
+        
+    }
+}
---
+++ LPR6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lansweeper6_PasswordRecovery
+{
+    class Program
+    {
+        static void usage()
+        {
+            Console.WriteLine("Usage: Lansweeper6_PasswordRecovery <key file> <encrypted password file>");
+        }
+
+        static void header()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            header();
+            if (args.Length != 2)
+            {
+                usage();
+                return;
+            }
+            Console.WriteLine(string.Format("[*] Processing files {0}, {1}.", args[0].ToString(), args[1].ToString()));
+
+            PasswordRecovery pr = new PasswordRecovery(args[0].ToString(), args[1].ToString());
+            pr.LoadFiles();
+            pr.DecryptAll();
+
+            Console.Write("Press any key...");
+            Console.ReadLine();
+        }
+    }
+}
